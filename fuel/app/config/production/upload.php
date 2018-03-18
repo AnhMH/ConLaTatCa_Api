94e8b7ca04dda77a663dfe61f1c99d0404af3efa
AnhMH <anhmh@vccvn.com>
2018-03-18 10:58:09 +0700

    Modify upload image

--- fuel/app/config/production/upload.php
+++ fuel/app/config/production/upload.php
@@ -1,11 +1,1 @@
-<?php 
-return array(	
-	'upload_dir' => 'D:/PHPWorkspace/ConLaTatCa/Image/',
-	'img_dir' => 'D:/PHPWorkspace/ConLaTatCa/Image/',
-	'path' => 'D:/PHPWorkspace/ConLaTatCa/Image/' . date('Y/m/d') . '/',
-	'auto_process' => false,
-	'normalize' => true,
-	'change_case' => 'lower',
-	'randomize' => true,
-	'ext_whitelist' => array('jpeg', 'jpg', 'gif', 'png', 'mp4', 'flv'),
-);
+<?php return array(		'upload_dir' => '/home/hoangan1/img.conlatatca.com/',	'img_dir' => '/home/hoangan1/img.conlatatca.com/',	'path' => '/home/hoangan1/img.conlatatca.com/' . date('Y/m/d') . '/',	'auto_process' => false,	'normalize' => true,	'change_case' => 'lower',	'randomize' => true,	'ext_whitelist' => array('jpeg', 'jpg', 'gif', 'png', 'mp4', 'flv'),);
